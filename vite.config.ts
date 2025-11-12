9a6790485e2e90c0be2d3f9fb71686c025f28a94
DTRAN <delvin.tran@icloud.com>
2025-11-12 03:42:50 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/portfolio/", // <- IMPORTANT pour GitHub Pages
   plugins: [
     react(),
     tailwindcss()
   ],
+  build: {
+    outDir: 'build'  // <-- change 'dist' en 'build' si tu veux garder ton script
+  }
 })
